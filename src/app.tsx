c15e94948dda6aa73ffd0699d67b8f3dbbfcf23c
Shevchenko Andrey <demorest49de@gmail.com>
2024-02-13 20:34:44 +0300

    Changes App.css App.module.css app.tsx index.css main.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import {Main} from './main';
 import {Nav} from "./nav";
 import s from "./App.module.css";
--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,7 @@ import picture from"./Assets/img/premium_photo-1669324357471-e33e71e3f3d8.avif"
 export const Main = () => {
     return (
         <main className={s.content}>
-            {/*<img src={picture} alt="picture" style={{objectFit: "cover", width: "100%"}} />*/}
-            <div style={{width: "100%", backgroundColor: 'beige', height: '170px', padding: '10px'}}>hello</div>
+            <img className={s.main__img} src={picture} alt="it"/>
         </main>
     );
 };
