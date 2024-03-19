3da83d0e5924efbcd3a2d4e4f94b76e53d42d4cc
Shevchenko Andrey <demorest49de@gmail.com>
2024-03-19 11:34:20 +0300

    Changes Messages.module.css Messages.tsx Post.tsx

--- src/Components/pages/messages/Messages.tsx
+++ src/Components/pages/messages/Messages.tsx
@@ -3,7 +3,26 @@ import React from 'react';
 export const Messages = () => {
     return (
         <div>
-            Messages
+            <div>
+                <div className={"user"}>
+                    Valera
+                </div>
+                <div className={"user"}>
+                    Marina
+                </div>
+                <div className={"user"}>
+                    Sveta
+                </div>
+                <div className={"user"}>
+                    Sasha
+                </div>
+            </div>
+            <div className={'messages'}>
+                <div className={'message'}>Hi</div>
+                <div className={'message'}>Hi</div>
+                <div className={'message'}>Hi</div>
+                <div className={'message'}>Hi</div>
+            </div>
         </div>
     );
 };
--- src/Components/pages/profile/myPosts/Post.tsx
+++ src/Components/pages/profile/myPosts/Post.tsx
@@ -4,7 +4,7 @@ import s from "../Profile.module.css";
 export const Post = () => {
     return (
         <div className={s.post__item}>
-            <img className={s.post_img} src={'https://ltdfoto.ru/images/2024/03/16/avatar.jpg'}/>
+            <img className={s.post_img} src={'https://ltdfoto.ru/images/2024/03/16/avatar.jpg'} alt={"picture"}/>
             <div>post 1</div>
             <div>
                 <span>like 1</span>
